9142b7d7556b6781f65ba1e2bde270e1dee8bcff
sergio <sergio@sergio-X556UAK>
2018-03-13 20:14:32 -0600

    pojo marca

---
+++ src/main/java/uesocc/ingenieria/pojosMantenimiento/Marca.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uesocc.ingenieria.pojosMantenimiento;
+
+/**
+ *
+ * @author sergio
+ */
+public class Marca {
+    private int id_marca;
+    private String Marca;
+
+    public Marca(int id_marca, String Marca) {
+        this.id_marca = id_marca;
+        this.Marca = Marca;
+    }
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(int id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+    
+    
+}
